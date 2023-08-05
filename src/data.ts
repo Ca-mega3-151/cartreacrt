81969ed7bf34dfa8dd77f9642eb0feb490a441f7
Nguyễn văn Huy <93319143+Ca-mega3-151@users.noreply.github.com>
2023-08-05 23:38:15 +0700

    a
    aaa

---
+++ src/data.ts
@@ -0,0 +1,42 @@
+
+const Items = [
+    {
+      id: 1,
+      title: "Áo thun",
+      src: "https://down-vn.img.susercontent.com/file/vn-11134207-7qukw-lixw7d64tmmqb5",
+      price: "12",
+      size: "xl",
+      color: "White",
+      discount: "5",
+    },
+    {
+      id: 2,
+      title: "Dép Khủng Long",
+      src: "https://down-vn.img.susercontent.com/file/sg-11134201-23010-k20p3glkitlv7f",
+      price: "9",
+      size: "43",
+      color: "White",
+      discount: "3",
+    },
+    {
+      id: 3,
+      title: "Mũ basic",
+      src: "https://down-vn.img.susercontent.com/file/a7bdeb1e942872a28c8772a36e93f186",
+      price: "15",
+      size: "xl",
+      color: "White",
+      discount: "2",
+    },
+    {
+      id: 4,
+      title: "Gấu Bông",
+      src: "https://down-vn.img.susercontent.com/file/sg-11134201-7qvfg-lfryn9q9wubl9a",
+      price: "20",
+      size: "xl",
+      color: "green",
+      discount: "6",
+    },
+
+]
+
+ export default Items;
